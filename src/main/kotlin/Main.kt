c003c27e8632ec88a73250771d305470b6d31ef2
suraj480 <surajmaurya991@gmail.com>
2023-11-08 11:51:01 +0530

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+    println("helo world")
+    printUserId()
+    printUserName()
+    printUserAddress()
+}
+fun printUserId(){
+    println("print user id")
+}
+fun printUserName(){
+    println("print user name")
+}
+fun printUserAddress(){
+    println("print user address")
+}
