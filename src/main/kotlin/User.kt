f111d05d5e5f32b078ca51ad5b0f495edc47399e
suraj480 <surajmaurya991@gmail.com>
2023-11-08 13:29:17 +0530

    basic

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,1 +1,19 @@
-fun main(){}
+fun main(){
+    var name= "suraj"
+    var userId=9823
+    var address="patiala"
+    println("User name is "+name)
+    println("user id is ${userId}")
+    println("user address is"+address)
+    print(" this is next line \n")
+    print("this is next to next line")
+    var a=10
+    var b=20
+println("value of a is ${a} , value of b is ${b} and sum of a and b is ${a+b}")
+    a= 3
+    b=5
+    println("value of a is ${a} , value of b is ${b} and sum of a and b is ${a+b}")
+
+    var firstName : String="suraj"
+
+}
